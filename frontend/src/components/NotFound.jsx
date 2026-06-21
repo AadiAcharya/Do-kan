509dd7eddddb38e2fad07eadaffba395ad01a13a
Sudipti Shah <153164055+sudiptishah@users.noreply.github.com>
2026-06-21 12:31:09 +0545

    Create NotFound.jsx

---
+++ frontend/src/components/NotFound.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function NotFound(){
+
+return(
+<div>
+<h1>404 - Page Not Found</h1>
+
+<p>The page does not exist</p>
+
+<Link to="/">
+Go Home
+</Link>
+
+</div>
+)
+
+}
+
+export default NotFound;
